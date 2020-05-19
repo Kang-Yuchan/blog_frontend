f448b57becc51a3576cc0c23a74ddda75133ca6e
Yuchan <mikuplayer123@gmail.com>
2020-05-20 07:14:03 +0900

    Connecting Post and API

--- js/index.js
+++ js/index.js
@@ -22,7 +22,9 @@ const buildPosts = (blogPosts) => {
   for (blogPost of blogPosts) {
     const postDate = new Date(parseInt(blogPost.added_date)).toDateString();
     const postImage = `${API_BASE_URL}${blogPost.post_image}`;
+    const postId = `/post.html?id=${blogPost.id}`;
     blogPostsContent += `
+    <a href="${postId}">
     <div class="post">
       <div class="post-image" style="background-image: url(${postImage})"></div>
         <div class="post-content">
@@ -31,6 +33,7 @@ const buildPosts = (blogPosts) => {
           <p class="post-text">${blogPost.content}</p>
         </div>
     </div>
+    </a>
     `;
   }
   document.querySelector(".main__container").innerHTML = blogPostsContent;
--- js/post.js
+++ js/post.js
@@ -3,17 +3,60 @@
  * TODO: Complete getPost function to get post data from API
  * TODO: Complete buildPost function to fill in the post data in the post.html file using ids
  */
-const API_URL = "http://localhost:3000/api/posts/";
-const API_BASE_URL = "http://localhost:3000/";
+const API_URL = "http://localhost:4000/api/posts/";
+const API_BASE_URL = "http://localhost:4000/";
 
 window.onload = () => {
   getPost();
+  getPostIdParam();
+};
+
+const getPostIdParam = () => {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get("id");
 };
 
 const getPost = () => {
   // CODE GOES HERE
+  const postId = getPostIdParam();
+  const url = `${API_URL}${postId}`;
+  fetch(url, {
+    method: "GET",
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((resData) => {
+      return buildPost(resData);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
-const buildPost = (data) => {
+const buildPost = (param) => {
   // HINT: Convert the date number to a Date string
+  let dataContent = "";
+  const postDate = new Date(parseInt(param.added_date)).toDateString();
+  const postImage = `${API_BASE_URL}${param.post_image}`;
+  dataContent += ` 
+      <header class="individual-banner" style="background-image: url(${postImage})"></header>
+      <div class="main">
+        <div class="main-container">
+          <div class="navigation">
+            <a href="index.html">
+              Back
+            </a>
+          </div>
+          <div class="post-container">
+          <div id="individual-post-title">${param.title}</div>
+          <div id="individual-post-date">${postDate}</div>
+          <div id="individual-post-content">${param.content}</div>
+          </div>
+        </div>
+      </div>
+    
+    `;
+  document.querySelector(".container").innerHTML = dataContent;
 };

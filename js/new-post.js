7c105d06ec366c539cd946b61405f90e9c0da2d8
Yuchan <mikuplayer123@gmail.com>
2020-05-20 10:25:48 +0900

    We are done

--- js/new-post.js
+++ js/new-post.js
@@ -1,10 +1,27 @@
 /**
- * TODO: Finish submitNewPost function to submit form data to the API 
+ * TODO: Finish submitNewPost function to submit form data to the API
  */
 
-const API_URL = "http://localhost:3000/api/posts";
+const API_URL = "http://localhost:4000/api/posts";
 
 const submitNewPost = () => {
-    // HINT: Use FormData to store data to send over
-    // HINT: Redirect the user to home page after successful submission
-}
+  // HINT: Use FormData to store data to send over
+  // HINT: Redirect the user to home page after successful submission
+  const title = document.getElementById("form-post-title").value;
+  const content = document.getElementById("form-post-content").value;
+  const postImage = document.getElementById("form-post-image");
+  console.log(postImage.files[0]);
+  let post = new FormData();
+  post.append("title", title);
+  post.append("content", content);
+  post.append("post_image", postImage.files[0]);
+
+  fetch(API_URL, {
+    method: "POST",
+    body: post,
+  }).then(() => {
+    setTimeout(() => {
+      window.location.href = "index.html";
+    }, 1000);
+  });
+};
